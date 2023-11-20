2553a69811e6ede698a0cc5b865ccf7b65cfcaec
souhai0x <souhailelbejjaj@gmail.com>
2023-11-20 14:44:20 -0800

    my first Website

---
+++ scripts/swiper.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+   
+    slidesPerView: 1.5, // Number of slides visible at a time
+    spaceBetween: 10, // Space between slides
+
+    autoplay: {
+      delay: 3000, // Autoplay delay in milliseconds (adjust as needed)
+    }
+    
+  });
+function takeMeGoogle(){
+  window.location.href = 'https://google.com'
+}
