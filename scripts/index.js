047b54cf6da0a253af6a0dbeeb7b883cb93b03e8
souhai0x <souhailelbejjaj@gmail.com>
2023-11-22 15:59:00 -0800

    Ajout de page recherche

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+function toSearch() {
+    window.location.href = 'pages/search-page.html'
+}
+function deletMostvisitedContainer() {
+}
+function displayMostvisitedContainer() {
+    document.querySelector('#most-visited-under-search-bar').style.display = 'block';
+    document.querySelector('#hook').style.display = 'block';
+}
+const input = document.querySelector("#search-bar");
+
+
+input.addEventListener("input", (e) => {
+    document.querySelector('#most-visited-under-search-bar').style.display = 'none';
+    document.querySelector('#hook').style.display = 'none';
+
+  
+});
